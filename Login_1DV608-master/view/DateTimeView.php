6291eefa0cb57a6514c3e81db51f31f3dbe895ef
pyrotecnick <lashinski@gmail.com>
2015-09-15 14:26:57 +0200

    Test case 1.1

--- Login_1DV608-master/view/DateTimeView.php
+++ Login_1DV608-master/view/DateTimeView.php
@@ -4,8 +4,30 @@ class DateTimeView {
 
 
 	public function show() {
+            
+                $data = getdate();
+                $weekday = $data['weekday'];
+                $mday = $data['mday'];
+                $month = $data['month'];
+                $year = $data['year'];
+                $hours = $data['hours'];
+                $minutes = $data['minutes'];
+                $seconds = $data['seconds'];
+                
 
-		$timeString = 'TODO, Write servertime here...';
+		$timeString = $weekday;
+                $timeString .= ', the ';
+                $timeString .= $mday;
+                $timeString .= 'th of ';
+                $timeString .= $month;
+                $timeString .=  ' ';
+                $timeString .= $year;
+                $timeString .=  ', The time is ';
+                $timeString .= $hours;
+                $timeString .= ':';
+                $timeString .= $minutes; 
+                $timeString .= ':';
+                $timeString .= $seconds;
 
 		return '<p>' . $timeString . '</p>';
 	}
